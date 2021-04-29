5e484ce46623713cf2cf6fa9d097f534da216725
alpaygunes <alpaygunes@gmail.com>
2021-04-29 09:43:09 +0300

    mesaj notification 01

--- functions/src/agnotification.ts
+++ functions/src/agnotification.ts
@@ -6,8 +6,7 @@ import * as admin from 'firebase-admin';
 export class AgNotification{
 
       ///////////////////////////////////////////  ORDER STATUS CHANGE ///////////////////////////////////
-      static OnOrderStatusUpdated      = functions.firestore.document('channels/{channelID}/orders/{orderID}')
-            .onUpdate(async (event,context) => { 
+      static OnOrderStatusUpdated      = functions.firestore.document('channels/{channelID}/orders/{orderID}').onUpdate(async (event,context) => { 
                   console.log("ORDER STATUS NOTİFİCATİON OrderStatusNotification onUpdate OLAYI TETİKLENDİ")  
                   const yeniData = event.after.data();
                   const eskiData = event.before.data();
@@ -62,41 +61,45 @@ export class AgNotification{
 
 
       ///////////////////////////////////////////  ON UPDATE OR CREATE NEW MESSAGE CHANGE ///////////////////////////////////
-      static OnMessageCreate      = functions.firestore.document('channels/{channelID}/messages/{messageID}')
-      .onCreate(async (event,context) => { 
+      static OnMessageCreate      = functions.firestore.document('channels/{channelID}/messages/{messageID}').onCreate(async (event,context) => { 
             console.log("YENİ MESAJ OLUŞTURULDU   channelID messageID status",context.params.channelID,context.params.messageID)
-            AgNotification.messageUpdateOrCreate(context.params.channelID,context.params.messageID,'update');
-      });      
-      static OnMessageUpdate      = functions.firestore.document('channels/{channelID}/messages/{messageID}')
-      .onUpdate(async (event,context) => { 
-            console.log("ESKİ MESAJ GÜNCELLENDİ channelID messageID status",context.params.channelID,context.params.messageID)
-            AgNotification.messageUpdateOrCreate(context.params.channelID,context.params.messageID,'create');
+            const message = event.data(); 
+            AgNotification.messageUpdateOrCreate(context.params.channelID,context.params.messageID,'create',message);
+      });   
+
+      static OnMessageUpdate      = functions.firestore.document('channels/{channelID}/messages/{messageID}').onUpdate(async (event,context) => { 
+            console.log("ESKİ MESAJ GÜNCELLENDİ channelID messageID status",context.params.channelID,context.params.messageID,null)
+            // !!!  mesajlarun okunma sayısı güncellenirken ayrıca tetiklendiği için update sırasında bildirimi devre dışı bıraktım.
+            //  reader_count update oluyorsa bildirim gene gönderiliyor. istersen durumu hariş tut diğer durumlarda bildirim gitsin.
+            //AgNotification.messageUpdateOrCreate(context.params.channelID,context.params.messageID,'create');
       });
 
 
-      static messageUpdateOrCreate(channelID:string,messageID:string,trigger_type:string) {
+      static messageUpdateOrCreate(channelID:string,messageID:string,trigger_type:string,message:any) {
             console.log("messageUpdateOrCreate tetiklendi ")
              // aboneleri azaltalım
              const tokens:string[] = [] 
              admin.firestore().collection('channels').doc(channelID).collection('subscribers').get().then(async data=>{
                   if(data.docs.length){  
 
-                        for await (let doc of data.docs) {
+                        for await (const doc of data.docs) {
                               const device = await admin.firestore().collection('devices').doc(doc.id).get() 
-                              console.log("******-----************ device.data()?.token,doc.id *******************",device.data()?.token,doc.id)
                               if(device.exists){
                                     tokens.push(device.data()?.token);
                               }
                         }
 
 
+                        const title       = (trigger_type === 'update') ? "İçerik güncellendi" : "Yeni mesajlarınız var";
+                        let   body        = "Değişiklikleri görmek için dokunun ";
+                        if(message.type === 'text'){
+                              body = message.text;
+                        }
 
-
-                        console.log(" -  tokenler ------- ",tokens)
                         const payload = {
                               notification:{
-                                    title       :(trigger_type === 'update')?"İçerik güncellendi":"Yeni mesajlarınız var",
-                                    body        :"Değişiklikleri görmek için dokunun ",
+                                    title       ,
+                                    body        ,
                                     badge       :"0",
                                     color       :"#ff0000",
                                     icon        :"default",
@@ -104,7 +107,8 @@ export class AgNotification{
                               },
                               data:{ 
                                     channelID,
-                                    messageID
+                                    messageID,
+                                    messg_type:message.type,
                               }
                         }
                         
@@ -112,6 +116,7 @@ export class AgNotification{
                               timeToLive: 86400,
                               priority: 'high',
                         }
+                        console.log("*************** notification" , JSON.stringify(payload))
                         admin.messaging().sendToDevice(tokens,payload,options).then(response=>{
                               console.log("Bildirim gönderildi:" /*,response*/);
                         }).catch(err=>{ 
